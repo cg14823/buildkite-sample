50d58659ad80af94b24951600c9f500d8bed44c9
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2021-08-03 10:06:58 +0100

    Fix pipeline

--- random/fns_test.go
+++ random/fns_test.go
@@ -9,12 +9,12 @@ import (
 func TestAdd(t *testing.T) {
 
 	tests := [][]int{
-		{ 1, 1, 2},
-		{ 1, -1, 0},
-		{ 1, 5, 1},
+		{1, 1, 2},
+		{1, -1, 0},
+		{-1, -5, -6},
 	}
 
-	for _, tc:= range tests {
+	for _, tc := range tests {
 		t.Run("sum", func(t *testing.T) {
 			require.Equal(t, AddInt(tc[0], tc[1]), tc[2])
 		})
