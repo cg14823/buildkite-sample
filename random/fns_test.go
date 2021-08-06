f2ab3797ac6ce46c7d8921d92a71e8dce38e91c7
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2021-08-06 08:38:41 +0100

    Introduce a flaky test

--- random/fns_test.go
+++ random/fns_test.go
@@ -1,13 +1,14 @@
 package random
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestAdd(t *testing.T) {
-
 	tests := [][]int{
 		{1, 1, 2},
 		{1, -1, 0},
@@ -18,5 +19,13 @@ func TestAdd(t *testing.T) {
 		t.Run("sum", func(t *testing.T) {
 			require.Equal(t, AddInt(tc[0], tc[1]), tc[2])
 		})
+	}
+}
+
+func TestFlacky(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if n := r.Intn(100); n > 75 {
+		t.Log(n)
+		t.Fail()
 	}
 }
