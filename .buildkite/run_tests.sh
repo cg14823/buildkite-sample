2125883b870e7ab52e08669a86d13d67f5ecb7e3
Carlos Gonzalez <cg14823@users.noreply.github.com>
2021-08-23 10:10:48 +0100

    Capture gotestsum exit code

--- .buildkite/run_tests.sh
+++ .buildkite/run_tests.sh
@@ -1,5 +1,10 @@
 echo "Running Go tests"
 gotestsum --junitfile test.xml
 
+exit_code=$?
+
+set -e
 echo "Uploading XML to Data Dog"
 DATADOG_API_KEY=$DATADOG_SECRET DD_ENV=carlos-testing DD_SITE=datad0g.com datadog-ci junit upload --service carlos-test test.xml
+
+exit exit_code
