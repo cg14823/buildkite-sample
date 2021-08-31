6717dcc70983cd0607b6642e751e1b51dfbc02ce
Carlos Gonzalez <cg14823@users.noreply.github.com>
2021-08-31 08:43:39 +0100

    Update run_tests.sh

--- .buildkite/run_tests.sh
+++ .buildkite/run_tests.sh
@@ -7,4 +7,5 @@ set -e
 echo "Uploading XML to Data Dog"
 DATADOG_API_KEY=$DATADOG_SECRET DD_ENV=carlos-testing DD_SITE=datad0g.com datadog-ci junit upload --service carlos-test test.xml
 
+echo "Exiting with ${exit_code}"
 exit ${exit_code}
